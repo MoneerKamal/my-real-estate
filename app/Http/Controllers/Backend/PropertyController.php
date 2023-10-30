00e75c9982960681b443a10a9b61167dbe0922c5
Moneer Kamal <MoneerKamalprog@gmail.com>
2023-10-30 14:04:49 +0300

    Update PropertyController.php

--- app/Http/Controllers/Backend/PropertyController.php
+++ app/Http/Controllers/Backend/PropertyController.php
@@ -127,4 +127,14 @@ class PropertyController extends Controller
         return redirect()->route('all.property')->with($notification);
 
     }
+    public function EditProperty($id){
+
+        $property = Property::findOrFail($id);
+        $propertytype = PropertyType::latest()->get();
+        $amenities = Amenities::latest()->get();
+        $activeAgent = User::where('status','active')->where('role','agent')->latest()->get();
+
+        return view('backend.property.edit_property',compact('property','propertytype','amenities','activeAgent'));
+
+    }
 }
