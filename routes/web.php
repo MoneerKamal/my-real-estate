3ed1f9b76480e5f0e8147aef4c5713dcad47ec72
Moneer Kamal <MoneerKamalprog@gmail.com>
2023-10-30 12:26:22 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -93,5 +93,6 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 
         Route::get('/all/property', 'AllProperty')->name('all.property');
         Route::get('/add/property', 'AddProperty')->name('add.property');
+        Route::post('/store/property', 'StoreProperty')->name('store.property');
     });
 });
