d2e692cdc38a0737d00416a65839af278b3473d6
Moneer Kamal <MoneerKamalprog@gmail.com>
2023-10-31 15:13:45 +0300

    Update PropertyController.php

--- app/Http/Controllers/Backend/PropertyController.php
+++ app/Http/Controllers/Backend/PropertyController.php
@@ -128,6 +128,7 @@ class PropertyController extends Controller
 
     }
     public function EditProperty($id){
+                $facilities = Facility::where('property_id',$id)->get();
  $property = Property::findOrFail($id);
 
         $type = $property->amenities_id;
@@ -139,7 +140,7 @@ class PropertyController extends Controller
         $amenities = Amenities::latest()->get();
         $activeAgent = User::where('status','active')->where('role','agent')->latest()->get();
 
-       return view('backend.property.edit_property',compact('property','propertytype','amenities','activeAgent','property_ami','multiImage'));
+       return view('backend.property.edit_property',compact('property','propertytype','amenities','activeAgent','property_ami','multiImage','facilities'));
 
     }
 
