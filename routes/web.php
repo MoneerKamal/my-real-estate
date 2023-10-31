bac1f9fae932167ed977f01cc209483a8d392b26
Moneer Kamal <MoneerKamalprog@gmail.com>
2023-10-31 15:46:07 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\AgentController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Backend\PropertyController;
 use App\Http\Controllers\Backend\PropertyTypeController;
+use App\Http\Middleware\RedirectIfAuthenticated;
 
 /*
 |--------------------------------------------------------------------------
@@ -64,7 +65,7 @@ Route::middleware(['auth', 'role:agent'])->group(function () {
 });
 
 // admin login
-Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login')->middleware(RedirectIfAuthenticated::class);
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
 
