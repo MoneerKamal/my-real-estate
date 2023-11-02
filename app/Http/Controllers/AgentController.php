7d577ea095b85ca8b10098e436090f4ece94083f
Moneer Kamal <MoneerKamalprog@gmail.com>
2023-11-02 12:04:42 +0300

    Update AgentController.php

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -22,4 +22,23 @@ class AgentController extends Controller
         return view('agent.agent_login');
 
     }
+    public function AgentRegister(Request $request){
+
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'password' => Hash::make($request->password),
+            'role' => 'agent',
+            'status' => 'inactive',
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect(RouteServiceProvider::AGENT);
+
+    }
 }
