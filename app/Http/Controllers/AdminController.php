f0e6103061e6055afa6535c8862bb2169d40709c
Moneer Kamal <MoneerKamalprog@gmail.com>
2023-11-02 15:04:57 +0300

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -116,4 +116,13 @@ class AdminController extends Controller
 
         return back()->with($notification);
     }
+
+     /////////// Agent User All Method ////////////
+
+  public function AllAgent(){
+
+    $allagent = User::where('role','agent')->get();
+    return view('backend.agentuser.all_agent',compact('allagent'));
+
+  }
 }
