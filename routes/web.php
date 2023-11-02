44f9f2079a4caebd11f0c844d6ac52a1a01dea21
Moneer Kamal <MoneerKamalprog@gmail.com>
2023-11-02 14:58:27 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -63,6 +63,8 @@ Route::middleware(['auth', 'role:agent'])->group(function () {
 
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
     Route::get('/agent/logout', [AgentController::class, 'AgentLogout'])->name('agent.logout');
+    Route::get('/agent/profile', [AgentController::class, 'AgentProfile'])->name('agent.profile');
+    Route::post('/agent/profile/store', [AgentController::class, 'AgentProfileStore'])->name('agent.profile.store');
 });
 Route::get('/agent/login', [AgentController::class, 'AgentLogin'])->name('agent.login')->middleware(RedirectIfAuthenticated::class);
 Route::post('/agent/register', [AgentController::class, 'AgentRegister'])->name('agent.register');
