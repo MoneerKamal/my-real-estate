07098cd1c1d4c38c95b1d475c1299a59776ecbf8
Moneer Kamal <MoneerKamalprog@gmail.com>
2023-11-03 12:48:34 +0300

    Update sidebar.blade.php

--- resources/views/agent/body/sidebar.blade.php
+++ resources/views/agent/body/sidebar.blade.php
@@ -1,3 +1,8 @@
+@php
+$id = Auth::user()->id;
+$agentId = App\Models\User::find($id);
+$status = $agentId->status;
+@endphp
 <nav class="sidebar">
       <div class="sidebar-header">
         <a href="#" class="sidebar-brand">
@@ -18,6 +23,7 @@
               <span class="link-title">Dashboard</span>
             </a>
           </li>
+            @if($status === 'active')
           <li class="nav-item nav-category">RealEstate</li>
 
            <li class="nav-item">
@@ -84,7 +90,9 @@
             </div>
           </li>
 
+ @else
 
+          @endif
 
 
 
